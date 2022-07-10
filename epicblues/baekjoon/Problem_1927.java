0b2324c3d8d86b0993138d32325a0f2ff59467a0
Minsung Kim <epicblue@hanmail.net>
2022-07-10 12:32:27 +0900

    [epiblues] 2022.7.10

--- epicblues/baekjoon/Problem_1927.java
+++ epicblues/baekjoon/Problem_1927.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Comparator;
 
-public class Main {
+public class Problem_1927 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
