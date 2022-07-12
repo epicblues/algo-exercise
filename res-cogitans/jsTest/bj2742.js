1ffc283ed265daa0e2f0d0beed75501b4d44e1d1
Hanbin Lee <logos1018@gmail.com>
2022-07-12 17:23:37 +0900

    4일차: 07월 12일 백준
    BJ 1259, 2562, 2742

---
+++ res-cogitans/jsTest/bj2742.js
@@ -0,0 +1,8 @@
+// let n = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString();
+const input = 5;
+console.log('aa');
+for (var i = input; i >= 0; i--) {
+    console.log(i);
+}
+console.log("ㅑㅑ")
