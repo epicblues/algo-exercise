e89f9ef12c445bf15d4a89c5ceb280ced725b482
Hanbin Lee <logos1018@gmail.com>
2022-07-15 02:52:41 +0900

    [res-cogitans] 6일차: 22.07.13
    백준 2751, 4153, 10814

---
+++ res-cogitans/javaTest/bj2751.java
@@ -0,0 +1,30 @@
+package javaTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class bj2751 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer> numbers = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            numbers.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(numbers);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : numbers) {
+            sb.append(i).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
---
+++ res-cogitans/javaTest/bj4153.java
@@ -0,0 +1,40 @@
+package javaTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class bj4153 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] length = new int[3];
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            length[0] = Integer.parseInt(st.nextToken());
+            if (length[0] == 0) {
+                break;
+            }
+            length[1] = Integer.parseInt(st.nextToken());
+            length[2] = Integer.parseInt(st.nextToken());
+
+            Arrays.sort(length);
+
+            if (isRATri(length)) {
+                System.out.println("right");
+            }
+            else {
+                System.out.println("wrong");
+            }
+        }
+    }
+
+    private static boolean isRATri(int[] length) {
+        if (length[0] * length[0] + length[1] * length[1] == length[2] * length[2]) {
+            return true;
+        }
+        return false;
+    }
+}
