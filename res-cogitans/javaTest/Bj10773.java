08cc1defed4c429873a3e94509e2de6b32f556d7
Hanbin Lee <logos1018@gmail.com>
2022-07-15 19:45:12 +0900

    백준 9012, 10773: 스택 관련

---
+++ res-cogitans/javaTest/Bj10773.java
@@ -0,0 +1,30 @@
+package javaTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class Bj10773 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		LinkedList<Integer> input = new LinkedList<>();
+		for (int i = 0; i < n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if (num == 0) {
+				input.removeLast();
+				continue;
+			}
+			input.add(num);
+		}
+
+		int sum = 0;
+		for (Integer i : input) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+}
